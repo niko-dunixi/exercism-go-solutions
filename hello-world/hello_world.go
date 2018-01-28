ec8860a47b7db91efb3e8878ae907247ff33fa8f
Paul Baker <paul.baker@wgu.edu>
2018-01-28 14:23:07 -0700

    Hello world, but overdone
    Decided to overdo it with a xor cypher. A really naive one.
    
    https://en.wikipedia.org/wiki/XOR_cipher

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -1,5 +1,12 @@
 package greeting
 
 func HelloWorld() string {
-	return "Hello, World!"
+	// Create and initialize a byte array
+	raw_bits := []byte{54, 27, 18, 18, 17, 82, 94, 41, 17, 12, 18, 26, 95}
+	// Iterate over every character and xor it with the numerical value of the character '~'
+	for i := 0; i < len(raw_bits); i++ {
+		raw_bits[i] = raw_bits[i] ^ '~'
+	}
+	// return the un-xor'd string that says hello world.
+	return string(raw_bits)
 }
