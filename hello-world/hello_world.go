889810874d371b49496c166449dc4d466d7c0485
Paul Baker <paul.baker@wgu.edu>
2018-01-28 14:37:21 -0700

    Fixing variable name
    Golang is MixedCase NOT snake_case

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -4,11 +4,11 @@ package greeting
 //	see: https://en.wikipedia.org/wiki/XOR_cipher
 func HelloWorld() string {
 	// Create and initialize a byte array
-	raw_bits := []byte{54, 27, 18, 18, 17, 82, 94, 41, 17, 12, 18, 26, 95}
+	rawBits := []byte{54, 27, 18, 18, 17, 82, 94, 41, 17, 12, 18, 26, 95}
 	// Iterate over every character and xor it with the numerical value of the character '~'
-	for i := 0; i < len(raw_bits); i++ {
-		raw_bits[i] = raw_bits[i] ^ '~'
+	for i := 0; i < len(rawBits); i++ {
+		rawBits[i] = rawBits[i] ^ '~'
 	}
 	// return the un-xor'd string that says hello world.
-	return string(raw_bits)
+	return string(rawBits)
 }
