e2b794249cb3da4fa80395ca5086030c92b325d6
Paul Baker <paul.baker@wgu.edu>
2018-01-28 15:41:40 -0700

    Adding documentation.

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -5,8 +5,11 @@ import (
 	"strings"
 )
 
-// Abbreviate should have a comment documenting it.
+// Abbreviate will take a string and return its acronym
 func Abbreviate(phrase string) string {
+	// 1 - Utilize a regex, at a given word boundary, take the first letter thereafter.
+	// 2 - Convert the input to uppercase and then run the phrase through the regex
+	// 3 - Join the strings without separation
 	re, _ := regexp.Compile("\\b(\\w)")
 	result := re.FindAllString(strings.ToUpper(phrase), -1)
 	return strings.Join(result, "")
