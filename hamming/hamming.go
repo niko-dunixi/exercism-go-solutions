387ac10bf371d5fc27e71a1d6df2ba534fbb9084
Paul Baker <paul.baker@wgu.edu>
2018-01-28 14:38:22 -0700

    Cleaning up old solution and restoring code

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,17 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("Strings must have the same length to determine hamming distance.")
+	}
+	totalDistance := 0
+	length := len(a)
+	for i := 0; i < length; i++ {
+		if a[i] != b[i] {
+			totalDistance++
+		}
+	}
+	return totalDistance, nil
 }
